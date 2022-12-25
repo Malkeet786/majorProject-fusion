6c4eb81c360059bbc15c400cb28a4f031ae108d2
Kiran6785 <kiranjeet.kaur@wittybrains.com>
2022-12-25 14:03:53 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import Main from "./components/main";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import SimpleForm from './components/simpleForm';
+
+export default _ => {
+  const [page, setPage] = useState(0);
+
+
+  let [showChat, setShowChat] = useState(false);
+
+  const startChat = () => { setShowChat(true); }
+  const hideChat = () => { setShowChat(false); }
+
+  return (
+    <div>
+      <Navbar setPage={setPage} />
+      {page === 0 ? <Main setPage={setPage} /> : null}
+
+      <Footer/>
+
+
+      <div className = "bot">
+        <div style ={{display: showChat ? "" : "none"}}>
+          <SimpleForm></SimpleForm>
+        </div>      
+        {/* <div> {showChat ? <SimpleForm></SimpleForm> : null} </div> */}
+        <div>
+          {!showChat 
+            ? <button className="chatbtn" onClick={() => startChat()}><i class="fab fa-forumbee"></i></button> 
+            : <button className="chatbtn" onClick={() => hideChat()}><i class="fab fa-forumbee"></i> </button>}
+        </div>
+      </div>      
+    </div>
+  );
+};
